29c63b11b20487a68a729a88bfb8d554d992b531
James Craig <4jcraig01@gmail.com>
2023-03-27 16:27:43 -0600

    remaps for controlling windows

--- lua/jcraig/remap.lua
+++ lua/jcraig/remap.lua
@@ -2,8 +2,18 @@
 vim.keymap.set("n", "<leader>pv", vim.cmd.NvimTreeFocus)
 vim.keymap.set("n", "<leader>fs", vim.cmd.w)
 
+-- Move highlighted lines up and down
 vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
 vim.keymap.set("v", "K", ":m '>-2<CR>gv=gv")
+
+-- Remaps for controlling windows
+vim.keymap.set("n","<leader>wv","<C-w>v") -- split vertical
+vim.keymap.set("n","<leader>ws","<C-w>s") -- split horizontal
+vim.keymap.set("n","<leader>wk","<C-w>k") -- Move to window above
+vim.keymap.set("n","<leader>wj","<C-w>j") -- Move to window below
+vim.keymap.set("n","<leader>wh","<C-w>h") -- Move to window left
+vim.keymap.set("n","<leader>wl","<C-w>l") -- Move to window right
+vim.keymap.set("n","<leader>wq","<C-w>q") -- Close current window
 
 -- Allows C-d and C-u to keep the cursor in the middle of the screen
 vim.keymap.set("n", "<C-d>", "<C-d>zz")
