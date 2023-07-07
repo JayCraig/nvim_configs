534a847d7e846c44782319dc6e04e10cb2b511c3
Jay Craig <4jcraig01@gmail.com>
2023-07-07 11:30:30 -0600

    updated remap.lua

--- lua/jcraig/remap.lua
+++ lua/jcraig/remap.lua
@@ -1,5 +1,5 @@
--- vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
-vim.keymap.set("n", "<leader>pv", vim.cmd.NvimTreeFocus)
+vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
+-- vim.keymap.set("n", "<leader>pv", vim.cmd.NvimTreeFocus)
 vim.keymap.set("n", "<leader>fs", vim.cmd.w)
 
 vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
